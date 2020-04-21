e22e0b89b2670bce64dcec9af4f8169598ddd3c5
Sam Balco <goodlyrottenapple@gmail.com>
2020-04-21 11:23:21 +0100

    fix class to className

--- frontend/src/components/EmptyBuilder.js
+++ frontend/src/components/EmptyBuilder.js
@@ -4,7 +4,7 @@ export default class EmptyBuilder extends React.Component {
 	
 	render() {
 		return (
-		  <div class={this.props.hrule ? "hrule": ""}>
+		  <div className={this.props.hrule ? "hrule": ""}>
 		  <h3 style={{paddingBottom: '0.5em'}}>{this.props.label?this.props.label:''}</h3>
 		  </div>
 		);
